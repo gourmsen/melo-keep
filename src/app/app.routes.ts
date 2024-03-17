88f49b8e8259b162c543672206de516f8f20c574
Maximilian Niessl <contact@maxniessl.com>
2024-03-17 18:43:02 +0100

    Added basic tab navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,8 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'repo',
-    loadComponent: () => import('./repo/repo.page').then( m => m.RepoPage)
-  },
-  {
-    path: 'hub',
-    loadComponent: () => import('./hub/hub.page').then( m => m.HubPage)
-  },
+    {
+        path: "",
+        loadChildren: () => import("./tabs/tabs.routes").then((m) => m.routes),
+    },
 ];
---
+++ src/app/tabs/tabs.routes.ts
@@ -0,0 +1,33 @@
+import { Routes } from "@angular/router";
+import { TabsComponent } from "./tabs.component";
+
+export const routes: Routes = [
+    {
+        path: "tabs",
+        component: TabsComponent,
+        children: [
+            {
+                path: "home",
+                loadComponent: () => import("../home/home.page").then((m) => m.HomePage),
+            },
+            {
+                path: "repo",
+                loadComponent: () => import("../repo/repo.page").then((m) => m.RepoPage),
+            },
+            {
+                path: "hub",
+                loadComponent: () => import("../hub/hub.page").then((m) => m.HubPage),
+            },
+            {
+                path: "",
+                redirectTo: "/tabs/home",
+                pathMatch: "full",
+            },
+        ],
+    },
+    {
+        path: "",
+        redirectTo: "/tabs/home",
+        pathMatch: "full",
+    },
+];
