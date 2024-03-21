41710a159cb5d1f16a11b87a26a70c8c62474573
Maximilian Niessl <contact@maxniessl.com>
2024-03-22 00:01:32 +0100

    Added version to settings tab

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,16 +1,22 @@
+// basic component
 import { Component, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
-import { IonContent, IonHeader, IonTitle, IonToolbar } from "@ionic/angular/standalone";
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonText } from "@ionic/angular/standalone";
+
+// functions
+import * as packageJSON from "../../../package.json";
 
 @Component({
     selector: "app-settings",
     templateUrl: "./settings.page.html",
     styleUrls: ["./settings.page.scss"],
     standalone: true,
-    imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule],
+    imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonText, CommonModule, FormsModule],
 })
 export class SettingsPage implements OnInit {
+    version: string = packageJSON.version;
+
     constructor() {}
 
     ngOnInit() {}
