04c2ba590fcf68131c7e3abbc4c1ea856f465e61
Maximilian Niessl <contact@maxniessl.com>
2024-03-31 18:12:15 +0200

    Set language on launch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+// basic component
+import { Component } from "@angular/core";
+import { IonApp, IonRouterOutlet } from "@ionic/angular/standalone";
+import { Subscription } from "rxjs";
+
+// services
+import { PreferencesService } from "./internal-services/preferences.service";
+import { TranslocoService } from "@jsverse/transloco";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+    selector: "app-root",
+    templateUrl: "app.component.html",
+    standalone: true,
+    imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+    languageSubscription: Subscription = new Subscription();
+
+    constructor(private preferences: PreferencesService, private transloco: TranslocoService) {}
+
+    ngOnInit() {
+        // set language
+        this.preferences
+            .getLanguage()
+            .then((result) => {
+                this.languageSubscription = this.preferences.languageSubject.subscribe((language) => {
+                    this.transloco.setActiveLang(language);
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    ngOnDestroy() {
+        this.languageSubscription.unsubscribe();
+    }
 }
